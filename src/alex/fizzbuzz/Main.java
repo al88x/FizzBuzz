086f5c1e9da9fd0922c67ffcb421423dddee056c
Alexandru Catarau <alexctrx@gmail.com>
2019-10-21 14:51:31 +0100

    Add first solution

--- src/alex/fizzbuzz/Main.java
+++ src/alex/fizzbuzz/Main.java
@@ -3,6 +3,22 @@ package alex.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+                continue;
+            }
+            if (i % 5 == 0) {
+                System.out.println("Buzz");
+                continue;
+            }
+            System.out.println(i);
+        }
+
     }
 }
