8e50063045f3b781bf022662612c6b2662b07e59
Alexandru Catarau <alexctrx@gmail.com>
2019-10-21 16:06:13 +0100

    Add Fezz rule

--- src/alex/fizzbuzz/Main.java
+++ src/alex/fizzbuzz/Main.java
@@ -4,10 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         StringBuilder word = new StringBuilder();
-        for (int i = 1; i <= 100; i++) {
+        boolean isFezz = false;
+        for (int i = 1; i <= 200; i++) {
+            if (i % 13 == 0) {
+                word.append("Fezz ");
+                isFezz = true;
+            }
             if (i % 11 == 0) {
-                System.out.println("Bong");
-                continue;
+                if (word.length() > 0) {
+                    word.append("Bong ");
+                } else {
+                    System.out.println("Bong ");
+                    continue;
+                }
             }
             if (i % 3 == 0) {
                 word.append("Fizz ");
@@ -19,13 +28,27 @@ public class Main {
                 word.append("Bang ");
             }
 
+            if (isFezz && wordContainsCharB(word.toString()) > 0) {
+                word.delete(0, 5);
+                word.insert(wordContainsCharB(word.toString()), "Fezz ");
+            }
             if (word.length() > 0) {
                 System.out.println(word.toString());
             } else {
                 System.out.println(i);
             }
+
             word.delete(0, word.length());
         }
 
+    }
+
+    private static int wordContainsCharB(String word) {
+        for (int j = 0; j < word.length(); j++) {
+            if (word.charAt(j) == 'B') {
+                return j;
+            }
+        }
+        return -1;
     }
 }
