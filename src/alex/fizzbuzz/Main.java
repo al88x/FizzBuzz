91d24517e2dfc80ae55254d811727b341fd014c5
Alexandru Catarau <alexctrx@gmail.com>
2019-10-21 15:35:03 +0100

    Refactor code + add Multiple of 11 and 7

--- src/alex/fizzbuzz/Main.java
+++ src/alex/fizzbuzz/Main.java
@@ -3,21 +3,28 @@ package alex.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-
+        StringBuilder word = new StringBuilder();
         for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
+            if (i % 11 == 0) {
+                System.out.println("Bong");
                 continue;
             }
             if (i % 3 == 0) {
-                System.out.println("Fizz");
-                continue;
+                word.append("Fizz ");
             }
             if (i % 5 == 0) {
-                System.out.println("Buzz");
-                continue;
+                word.append("Buzz ");
             }
-            System.out.println(i);
+            if (i % 7 == 0) {
+                word.append("Bang ");
+            }
+
+            if (word.length() > 0) {
+                System.out.println(word.toString());
+            } else {
+                System.out.println(i);
+            }
+            word.delete(0, word.length());
         }
 
     }
